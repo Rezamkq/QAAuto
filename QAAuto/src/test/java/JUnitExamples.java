389437c4bfa9d769a36450fbcd700e31e6f5a4ba
Rezam <Rezamkq@2140@gmail.com>
2025-06-20 12:56:25 +0400

    upd3

---
+++ QAAuto/src/test/java/JUnitExamples.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitExamples {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("After Each");
+    }
+
+    @Test
+    void firstTest(){
+        System.out.println("firstTest");
+    }
+    @Test
+    void secondTest(){
+        System.out.println("serTest");
+    }
+}
